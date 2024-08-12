643b9f3cc559da0e1de61fd0aea4f7299f7b3631
linuseing <linus@r13.de>
2024-08-12 18:36:40 +0200

    JWT_KEY as env var

--- src/auth_utils.py
+++ src/auth_utils.py
@@ -14,7 +14,7 @@ from . import repositories
 from . import models
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+SECRET_KEY = os.environ.get("JWT_KEY")
 
 
 def create_access_token(data: dict, key: str, expires_delta: Union[datetime.timedelta, None] = None):
