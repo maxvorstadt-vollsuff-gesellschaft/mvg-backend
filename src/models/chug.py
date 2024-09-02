ca93114df96ad2b7abcbf9a2bfe52cb9f067856a
linuseing <linus@r13.de>
2024-09-02 15:04:05 +0200

    chugging api

--- src/models/chug.py
+++ src/models/chug.py
@@ -2,9 +2,10 @@ from sqlalchemy import Integer, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .member import Member
+from .base import Base
 
 
-class Chug:
+class Chug(Base):
     __tablename__ = "chugs"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
