05d39f27ee67a83cd6ec655261e80a3beca1c93b
linuseing <linus@r13.de>
2024-09-03 19:17:01 +0200

    chuging api

--- src/repositories/chug.py
+++ src/repositories/chug.py
@@ -1,8 +1,24 @@
+from sqlalchemy import func
+
 from .base import CRUDBase
 from ..models import Chug
 
 
 class CRUDChug(CRUDBase[Chug]):
-    pass
+    def get_ordered(self, db, limit=10):
+        return db.query(self.model).order_by(Chug.time.asc()).limit(limit).all()
+
+    def get_ordered_unique(self, db, limit=10):
+        subquery = db.query(
+            Chug.member_id,
+            func.min(Chug.time).label('min_time')
+        ).group_by(Chug.member_id).subquery()
+
+        result = db.query(Chug).join(
+            subquery,
+            (Chug.member_id == subquery.c.member_id) & (Chug.time == subquery.c.min_time)
+        ).order_by(Chug.time.asc()).limit(limit).all()
+    
+        return result
 
 chug_repository = CRUDChug(Chug)
--- src/routes/chug.py
+++ src/routes/chug.py
@@ -23,3 +23,9 @@ def create_event(
 ):
     for i in range(len(chugs.id)):
         repositories.chug_repository.create(db, {"member_id": chugs.id[i], "time": chugs.time[i]})
+
+
+@router.get("/top-player")
+def top_player(limit: int = 10, db=Depends(get_db)) -> list[schemas.BaseChug]:
+    db_chugs = repositories.chug_repository.get_ordered_unique(db, limit)
+    return [schemas.BaseChug.model_validate(chug) for chug in db_chugs]
