426188e817580a9c240a88bf57886c06373b5f38
Linus Eing <Linus@r13.de>
2024-11-06 16:07:48 +0100

    xd

--- src/schemas/recipe.py
+++ src/schemas/recipe.py
@@ -3,14 +3,13 @@ from typing import Optional, List
 
 from pydantic import BaseModel, Field
 from .member import Member  
+from ..models.recipe import TimeOfDay, ComplexityLevel
 
 class RecipeBase(BaseModel):
     name: str
     description: Optional[str] = Field(None, description="A brief description of the recipe.")
-    ingredients: List[str] = Field(..., description="List of ingredients required for the recipe.")
-    instructions: str = Field(..., description="Step-by-step instructions for the recipe.")
-    prep_time: Optional[int] = Field(None, description="Preparation time in minutes.")
-    cook_time: Optional[int] = Field(None, description="Cooking time in minutes.")
+    time_of_day: TimeOfDay
+    complexity: Optional[ComplexityLevel]
 
 class RecipeCreate(RecipeBase):
     author_id: Optional[int]
@@ -20,8 +19,6 @@ class RecipeUpdate(RecipeBase):
 
 class Recipe(RecipeBase):
     id: int
-    created_at: datetime
-    updated_at: datetime
     author: Optional[Member]
 
     class Config:
