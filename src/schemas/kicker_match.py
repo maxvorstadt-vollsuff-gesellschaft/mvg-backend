d51d7e572fce2bb785f18335aaa9b111a7c2edc9
Linus Eing <Linus@r13.de>
2024-12-18 15:40:20 +0100

    return member

--- src/schemas/kicker_match.py
+++ src/schemas/kicker_match.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel
 from typing import Optional
 
+from .member import Member
+
 class KickerMatchBase(BaseModel):
     team_a_score: int
     team_b_score: int
@@ -14,6 +16,10 @@ class KickerMatchCreate(KickerMatchBase):
 
 class KickerMatch(KickerMatchBase):
     id: int
+    team_a_player_1_member: Member
+    team_a_player_2_member: Optional[Member] = None
+    team_b_player_1_member: Member
+    team_b_player_2_member: Optional[Member] = None
 
     class Config:
         from_attributes = True
