d6875723dffff8d70b57d7d26f4d0bc015fa8e09
Linus Eing <Linus@r13.de>
2024-12-29 14:32:01 +0100

    right game ordering

--- src/repositories/kicker.py
+++ src/repositories/kicker.py
@@ -1,4 +1,4 @@
-from typing import Annotated
+from typing import Annotated, List
 from fastapi import Depends
 from sqlalchemy.orm import Session
 
@@ -10,6 +10,9 @@ from ..database import get_db
 class CRUDKickerMatch(CRUDBase[KickerMatch]):
     def __init__(self, db: Annotated[Session, Depends(get_db)]):
         super().__init__(KickerMatch, db)
+
+    def get_multi(self, skip: int = 0, limit: int = 10) -> List[KickerMatch]:
+        return self.db.query(KickerMatch).order_by(KickerMatch.start_time.desc()).offset(skip).limit(limit).all()
 
 
 def get_kicker_match_repository(db: Annotated[Session, Depends(get_db)]) -> CRUDKickerMatch:
